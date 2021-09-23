8cb586fb8f593d6817fe4d2922d8dfb6acd458dd
Vladyslav Lytvynenko <vlad.lytvynenko12@gmail.com>
2021-09-23 22:04:44 +0300

    add _document

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Head, Html, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initialProps = await Document.getInitialProps(ctx);
+		return initialProps;
+	}
+
+	render(): JSX.Element {
+		return (
+			<Html>
+				<Head />
+				<body />
+				<Main />
+				<NextScript />
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
