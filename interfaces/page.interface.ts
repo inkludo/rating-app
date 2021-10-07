5ff2205e3a2a578253c39c9f22f968377b9ea1fb
Vladyslav Lytvynenko <vlad.lytvynenko12@gmail.com>
2021-10-07 23:34:30 +0300

    add context

---
+++ interfaces/page.interface.ts
@@ -0,0 +1,40 @@
+export enum LevelCategory {
+	Courses,
+	Services,
+	Books,
+	Products
+}
+
+export interface PageAdvantage {
+	_id: string;
+	title: string;
+	description: string;
+}
+
+export interface HhData {
+	_id: string;
+	count: number;
+	juniorSalary: number;
+	middleSalary: number;
+	seniorSalary: number;
+	updatedAt: Date;
+}
+
+export interface PageModel {
+	tags: string[];
+	_id: string;
+	secondCategory: string;
+	alias: string;
+	title: string;
+	category: string;
+	seoText?: string;
+	tagsTitle: string;
+	metaTitle: string;
+	metaDescription: string;
+	firstCategory: LevelCategory;
+	advantages?: PageAdvantage[];
+	createdAt: Date;
+	updatedAt: Date;
+	hh?: HhData;
+}
+
