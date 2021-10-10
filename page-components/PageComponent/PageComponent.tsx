48ace78c3c90aaff7a8fb1f0710ae199026b4b19
Vladyslav Lytvynenko <vlad.lytvynenko12@gmail.com>
2021-10-10 21:11:13 +0300

    add new component and fix styles

--- page-components/PageComponent/PageComponent.tsx
+++ page-components/PageComponent/PageComponent.tsx
@@ -1,11 +1,12 @@
-import { Htag, Tag, HhData } from '../../components';
+import { Htag, Tag, HhData, Advantages } from '../../components';
 import { PageComponentProps } from './PageComponent.props';
-import styles from './PageComponent.module.css';
 import { LevelCategory } from '../../interfaces/page.interface';
+import parse from 'html-react-parser';
+import styles from './PageComponent.module.css';
 
 export const PageComponent = ({ page, products, firstCategory }: PageComponentProps): JSX.Element => {
 	return (
-		<div className={styles.wrapper}>
+		<div>
 			<div className={styles.title}>
 				<Htag tag='h1'>{page.title}</Htag>
 				{products && <Tag color='grey' size='m'>{products.length}</Tag>}
@@ -15,11 +16,19 @@ export const PageComponent = ({ page, products, firstCategory }: PageComponentPr
 				{products && products.map(product => <div key={product._id}>{product.title}</div>)}
 			</div>
 			<div className={styles.hhTitle}>
-				<Htag tag='h2'>Vacancies – {page.category}</Htag>
+				<Htag tag='h2'>Вакансии – {page.category}</Htag>
 				<Tag color='red' size='m'>hh.ru</Tag>
 			</div>
 			<div className={styles.hh}>
 				{firstCategory == LevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+				{page.advantages && page.advantages.length > 1 && <>
+					<Htag tag='h2'>Преимущества</Htag>
+					<Advantages advantages={page.advantages} />
+				</>
+				}
+				{page.seoText && <div className={styles.seo}>{parse(page.seoText)}</div>}
+				<Htag tag='h2'>Получаемые навыки</Htag>
+				{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
 			</div>
 		</div>
 	);
--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -1,23 +1,20 @@
-import { GetStaticPropsContext, GetStaticProps, GetStaticPaths } from 'next';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import React from 'react';
 import { withLayout } from '../../layout/Layout';
 import axios from 'axios';
 import { MenuItem } from '../../interfaces/menu.interface';
 import { LevelCategory, PageModel } from '../../interfaces/page.interface';
-import { ParsedUrlQuery } from 'querystring';
+import { ParsedUrlQuery } from 'node:querystring';
 import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
 import { PageComponent } from '../../page-components';
 
-
-function Page({ firstCategory, products, page }: PageProps): JSX.Element {
-
-	return (
-		<PageComponent
-			firstCategory={firstCategory}
-			products={products}
-			page={page}
-		/>
-	);
+function Page({ firstCategory, page, products }: PageProps): JSX.Element {
+	return <PageComponent
+		firstCategory={firstCategory}
+		page={page}
+		products={products}
+	/>;
 }
 
 export default withLayout(Page);
@@ -28,9 +25,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 		const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
 			firstCategory: m.id
 		});
-		paths = paths.concat(menu.flatMap(s => s.pages.map(p => `/{${m.route}}/${p.alias}`)));
+		paths = paths.concat(menu.flatMap(s => s.pages.map(p => `/${m.route}/${p.alias}`)));
 	}
-
 	return {
 		paths,
 		fallback: true
@@ -43,26 +39,21 @@ export const getStaticProps: GetStaticProps<PageProps> = async ({ params }: GetS
 			notFound: true
 		};
 	}
-
 	const firstCategoryItem = firstLevelMenu.find(m => m.route == params.type);
-
 	if (!firstCategoryItem) {
 		return {
 			notFound: true
 		};
 	}
-
 	try {
 		const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
 			firstCategory: firstCategoryItem.id
 		});
-
 		if (menu.length == 0) {
 			return {
 				notFound: true
 			};
 		}
-
 		const { data: page } = await axios.get<PageModel>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/byAlias/' + params.alias);
 		const { data: products } = await axios.post<ProductModel[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/product/find', {
 			category: page.category,
@@ -72,9 +63,9 @@ export const getStaticProps: GetStaticProps<PageProps> = async ({ params }: GetS
 		return {
 			props: {
 				menu,
+				firstCategory: firstCategoryItem.id,
 				page,
-				products,
-				firstCategory: firstCategoryItem.id
+				products
 			}
 		};
 	} catch {
@@ -82,7 +73,6 @@ export const getStaticProps: GetStaticProps<PageProps> = async ({ params }: GetS
 			notFound: true
 		};
 	}
-
 
 };
 
