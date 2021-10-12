266e8a11597b6a7de53c4ca3e528417ff13b6c62
Vladyslav Lytvynenko <vlad.lytvynenko12@gmail.com>
2021-10-12 17:54:45 +0300

    add features in Product component

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,11 +1,10 @@
 import { ProductProps } from './Product.props';
 import styles from './Product.module.css';
-import cn from 'classnames';
 import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
-import { priceUSD } from '../../helpers/helpers';
+import { declOfNum, priceUSD } from '../../helpers/helpers';
 import { Divider } from '..';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
@@ -25,10 +24,18 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 			<div className={styles.tags}>{product.categories.map(c => <Tag key={c} className={styles.category} color='ghost'>{c}</Tag>)}</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
-			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+			<div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
 			<Divider className={styles.hr} />
 			<div className={styles.description}>{product.description}</div>
-			<div className={styles.feature}>фичи</div>
+			<div className={styles.feature}>
+				{product.characteristics.map(c => (
+					<div className={styles.characteristics} key={c.name}>
+						<span className={styles.characteristicsName}>{c.name}</span>
+						<span className={styles.characteristicsDot}></span>
+						<span className={styles.characteristicsValue}>{c.value}</span>
+					</div>
+				))}
+			</div>
 			<div className={styles.advBlock}>
 				{product.advantages && <div className={styles.advantages}>
 					<div className={styles.advTitle}>Преимущества</div>
