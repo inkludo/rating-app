57bb2ab36a2fccf7c0c9819c6e1b1540f2d4ff1b
Vladyslav Lytvynenko <vlad.lytvynenko12@gmail.com>
2021-10-13 21:37:49 +0300

    fix bug with sort

--- page-components/PageComponent/sort.reducer.ts
+++ page-components/PageComponent/sort.reducer.ts
@@ -13,12 +13,12 @@ export const sortReducer = (state: SortReducerState, action: SortActions): SortR
 		case SortEnum.Rating:
 			return {
 				sort: SortEnum.Rating,
-				products: state.products.sort((a, b) => a.initialRating - b.initialRating)
+				products: state.products.sort((a, b) => a.initialRating > b.initialRating ? -1 : 1)
 			};
 		case SortEnum.Price:
 			return {
 				sort: SortEnum.Price,
-				products: state.products.sort((a, b) => a.price - b.price)
+				products: state.products.sort((a, b) => a.price > b.price ? 1 : -1)
 			};
 		case 'reset':
 			return {
